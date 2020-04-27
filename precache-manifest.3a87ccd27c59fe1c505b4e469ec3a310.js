3096812b182a85115ebbd87dd877fad1e556c266
nitin <nshukla6@gmail.com>
2020-04-28 00:32:08 +0530

    Updates

---
+++ precache-manifest.3a87ccd27c59fe1c505b4e469ec3a310.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d2f8ee35e9b96cb1dad1a60166bad7",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "407bc607cfe75d88f646",
+    "url": "/covid-19/static/css/main.515a5f5f.chunk.css"
+  },
+  {
+    "revision": "cf2c63645544d2f02cb6",
+    "url": "/covid-19/static/js/2.58f14cd1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.58f14cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407bc607cfe75d88f646",
+    "url": "/covid-19/static/js/main.8ceb68a6.chunk.js"
+  },
+  {
+    "revision": "27851c564b72b60e68b3",
+    "url": "/covid-19/static/js/runtime-main.7f400c4a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
