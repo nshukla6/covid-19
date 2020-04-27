fed849be2a859191a92db42b63d3f33f7d60a1cf
nitin <nshukla6@gmail.com>
2020-04-28 00:45:34 +0530

    added responsive ui changes

---
+++ build/precache-manifest.66d17f0d593558858f92014ab3cbb5c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ac7167719179c886276d0253d5f1f4",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d774a009520e3f244874",
+    "url": "/covid-19/static/css/main.8b1a3d16.chunk.css"
+  },
+  {
+    "revision": "cf2c63645544d2f02cb6",
+    "url": "/covid-19/static/js/2.58f14cd1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.58f14cd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d774a009520e3f244874",
+    "url": "/covid-19/static/js/main.8ceb68a6.chunk.js"
+  },
+  {
+    "revision": "27851c564b72b60e68b3",
+    "url": "/covid-19/static/js/runtime-main.7f400c4a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
